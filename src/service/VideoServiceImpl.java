6d60d4ff555fecb5c7af3fbc13d033202b61daca
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-30 06:36:53 -0300

    Add validations to addVideo and implemented listVideos

--- src/service/VideoServiceImpl.java
+++ src/service/VideoServiceImpl.java
@@ -14,11 +14,23 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public void addVideo(Video video) {
+        if (video == null) {
+            throw new IllegalArgumentException("O video não pode ser nulo");
+        }
+        if (video.getTitulo() == null || video.getTitulo().isBlank()) {
+            throw new IllegalArgumentException("O Título do video não pode ser nulo ou branco");
+        }
+        if (video.getDescricao() == null || video.getDescricao().isBlank()) {
+            throw new IllegalArgumentException("A descrição do video não pode ser nulo ou branco");
+        }
+        if (video.getDuracao() <= 0) {
+            throw new IllegalArgumentException("A duração do video não pode ser menor que zero");
+        }
         repository.save(video);
     }
 
-    @Override
-    public List<Video> listVideos() {
-        return repository.findAll();
+        @Override
+        public List<Video> listVideos () {
+            return repository.findAll();
+        }
     }
-}
