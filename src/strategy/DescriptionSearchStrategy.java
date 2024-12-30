80d151d87bb0574fa6de51e78ab44509e2874af2
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-30 07:44:52 -0300

    Implement search strategies for title, description, and category

--- src/strategy/DescriptionSearchStrategy.java
+++ src/strategy/DescriptionSearchStrategy.java
@@ -5,11 +5,11 @@ import model.Video;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TitleSearchStrategy implements SearchStrategy {
+public class DescriptionSearchStrategy implements SearchStrategy {
     @Override
     public List<Video> search(List<Video> videos, String query) {
         return videos.stream()
-                .filter(video -> video.getTitulo().toLowerCase().contains(query.toLowerCase()))
+                .filter(video -> video.getDescricao().toLowerCase().contains(query.toLowerCase()))
                 .collect(Collectors.toList());
     }
 }
